147cbd6577b52e826b58c0a42d8cf544c4f8357e
gilbert-keter <gilbert@cw.co.ke>
2025-03-27 18:43:27 +0300

    Bug fixes

--- app/components/ui/sidebar.tsx
+++ app/components/ui/sidebar.tsx
@@ -196,7 +196,7 @@ const Sidebar = React.forwardRef<
           <SheetContent
             data-sidebar="sidebar"
             data-mobile="true"
-            className="w-[--sidebar-width] bg-white bg-white p-0 text-sidebar-foreground dark:bg-sidebar [&>button]:hidden h-screen" // Added h-screen
+            className="w-[--sidebar-width] bg-white bg-white p-0 text-sidebar-foreground dark:bg-sidebar [&>button]:hidden h-full"
             style={
               {
                 "--sidebar-width": SIDEBAR_WIDTH_MOBILE,
@@ -204,8 +204,7 @@ const Sidebar = React.forwardRef<
             }
             side={side}
           >
-            <div className="flex h-screen w-full flex-col">{children}</div> //
-            Changed h-full to h-screen
+            <div className="flex h-full w-full flex-col">{children}</div>
           </SheetContent>
         </Sheet>
       );
